548fac760fbb9d2a282060aa282f34d88f6e61c2
Vincentas Olsauskas <vinnyolsauskas@Vinnys-MacBook-Pro.local>
2026-03-01 10:00:44 -0600

    fix resend init

--- app/api/send-report/route.js
+++ app/api/send-report/route.js
@@ -1,9 +1,8 @@
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function POST(request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY)
     const { name, email, filename, report } = await request.json()
 
     await resend.emails.send({
